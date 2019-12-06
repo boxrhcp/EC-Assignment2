da4795c3bb8d40d742b46fc7ec5e0e70dab7b987
Fayan Leonardo Pardo <fayanpardo@gmail.com>
2019-12-06 13:43:20 +0100

    communication works

--- src/main/java/de/tub/ise/QuorumImpl.java
+++ src/main/java/de/tub/ise/QuorumImpl.java
@@ -1,18 +1,24 @@
 package de.tub.ise;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
+import de.tub.ise.KeyValueStoreGrpc.KeyValueStoreStub;
+import de.tub.ise.Response;
+import io.grpc.stub.StreamObserver;
+import io.grpc.*;
 import org.apache.log4j.Logger;
 //import de.tub.ise.KeyValuePair;
 
-
-import io.grpc.*;
 
 public class QuorumImpl extends KeyValueStoreGrpc.KeyValueStoreImplBase {
     private final int qwritesize;
     private final int qreadsize;
     private final HashMap<String, String> otherNodes;
-    private final HashMap<String, NodeClient> otherServer;
+    private final HashMap<String, KeyValueStoreStub> otherServer;
     static Logger logger = Logger.getLogger(QuorumImpl.class.getName());
 
     /**
@@ -29,7 +35,8 @@ public class QuorumImpl extends KeyValueStoreGrpc.KeyValueStoreImplBase {
             int port = Integer.parseInt(entry.getValue().split(":")[1]);
             // TODO create async stubs for communication between nodes
             logger.info(host + ":" + port);
-            otherServer.put(node, new NodeClient(host,port));
+            otherServer.put(node, KeyValueStoreGrpc.newStub(ManagedChannelBuilder.forAddress(host, port)
+                    .usePlaintext().build()));
         }
 
     }
@@ -41,7 +48,7 @@ public class QuorumImpl extends KeyValueStoreGrpc.KeyValueStoreImplBase {
      */
     @Override
     public void put(de.tub.ise.KeyValuePair request,
-            io.grpc.stub.StreamObserver<de.tub.ise.Response> responseObserver) {
+                    io.grpc.stub.StreamObserver<de.tub.ise.Response> responseObserver) {
         String key = request.getKey();
         String value = request.getValue();
         Response response;
@@ -70,7 +77,7 @@ public class QuorumImpl extends KeyValueStoreGrpc.KeyValueStoreImplBase {
         String key = request.getKey();
         Response response;
 
-        logger.debug("Received get request with key" + key);
+        logger.debug("Received get request with key " + key);
 
         KeyValuePair data = gatherdata(key);
         if (data == null) {
@@ -107,21 +114,27 @@ public class QuorumImpl extends KeyValueStoreGrpc.KeyValueStoreImplBase {
      */
     @Override
     public void replicate(de.tub.ise.KeyValuePair request,
-            io.grpc.stub.StreamObserver<de.tub.ise.Response> responseObserver) {
-        // TODO handle replication requests from other nodes
-        String key = request.getKey();
-        String value = request.getValue();
-
+                          io.grpc.stub.StreamObserver<de.tub.ise.Response> responseObserver) {
+        // handle replication requests from other nodes
+        final String key = request.getKey();
+        final String value = request.getValue();
         Response response;
 
-        logger.debug("Received replicate request with key " + key);
-        Memory.put(key, value);
-
-        response = Response.newBuilder().setSuccess(true).setKey(key).build();
-        logger.debug("Telling the node that we replicated");
-
-        responseObserver.onNext(response);
-        responseObserver.onCompleted();
+        // Set ctx as the current context within the Runnable
+        Context forked = Context.current().fork();
+        Context old = forked.attach();
+        try {
+            logger.debug("Received replicate request with key " + key);
+            Memory.put(key, value);
+            response = Response.newBuilder().setSuccess(true).setKey(key).build();
+            logger.debug("Telling the node that we replicated");
+            responseObserver.onNext(response);
+        }catch(Exception e){
+            responseObserver.onError(e);
+        } finally {
+            forked.detach(old);
+            responseObserver.onCompleted();
+        }
     }
 
     /**
@@ -130,23 +143,30 @@ public class QuorumImpl extends KeyValueStoreGrpc.KeyValueStoreImplBase {
      */
     @Override
     public void getReplica(de.tub.ise.Key request, io.grpc.stub.StreamObserver<de.tub.ise.Response> responseObserver) {
-        // TODO handle request for local replica from other nodes
+        //handle request for local replica from other nodes
         String key = request.getKey();
         Response response;
-
+        String data = null;
         logger.debug("Received getReplica request with key" + key);
 
-        KeyValuePair data = gatherdata(key);
-        if (data == null) {
-            response = Response.newBuilder().setSuccess(false).setKey(key).build();
-            logger.warn("Uh oh, couldn't get data :(");
-        } else {
-            response = Response.newBuilder().setSuccess(true).setKey(data.getKey()).setValue(data.getValue()).build();
-            logger.debug("Giving node the requested replica data");
+        Context forked = Context.current().fork();
+        Context old = forked.attach();
+        try {
+            data = Memory.get(key);
+            if (data == null) {
+                response = Response.newBuilder().setSuccess(false).setKey(key).build();
+                logger.warn("Uh oh, couldn't get data :(");
+            } else {
+                response = Response.newBuilder().setSuccess(true).setKey(key).setValue(data).build();
+                logger.debug("Giving node the requested replica data for key: " + key);
+            }
+            responseObserver.onNext(response);
+        }catch(Exception e){
+            responseObserver.onError(e);
+        } finally {
+            forked.detach(old);
+            responseObserver.onCompleted();
         }
-
-        responseObserver.onNext(response);
-        responseObserver.onCompleted();
     }
 
     /**
@@ -155,7 +175,7 @@ public class QuorumImpl extends KeyValueStoreGrpc.KeyValueStoreImplBase {
      */
     @Override
     public void deleteReplica(de.tub.ise.Key request,
-            io.grpc.stub.StreamObserver<de.tub.ise.Response> responseObserver) {
+                              io.grpc.stub.StreamObserver<de.tub.ise.Response> responseObserver) {
         // TODO handle delete requests from other nodes, akin to write requests
         String key = request.getKey();
         Memory.delete(key);
@@ -167,48 +187,131 @@ public class QuorumImpl extends KeyValueStoreGrpc.KeyValueStoreImplBase {
 
     /**
      * Method to check if quorum replication has been achieved.
-     * 
+     * <p>
      * You are free to change this method as you see fit
      */
     private boolean replicateData(String key, String value) {
-
         // Write key-value pair in-memory
         Memory.put(key, value);
+        KeyValuePair request = KeyValuePair.newBuilder().setKey(key).setValue(value).build();
+        List<Response> results = new ArrayList<Response>();
+        final CountDownLatch finishLatch = new CountDownLatch(qwritesize - 1);
+        StreamObserver<Response> responses = new StreamObserver<Response>() {
+            @Override
+            public void onNext(Response response) {
+                logger.info("Replica written " + response.getSuccess());
+                if (response.getSuccess()) results.add(response);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                logger.warn("Write replica failed: " + t.getMessage());
+                finishLatch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                logger.info("Finished replicate");
+                finishLatch.countDown();
+            }
+        };
+
+        // send async. replication requests to nodes
+        for (HashMap.Entry<String, KeyValueStoreStub> entry : otherServer.entrySet()) {
+            entry.getValue().replicate(request, responses);
+        }
 
         if (qwritesize > 1) {
-            // TODO send async. replication requests to nodes
-            for (HashMap.Entry<String, NodeClient> entry : otherServer.entrySet()) {
-                entry.getValue().replicate(key,value);
+            try {
+                // Wait until minimum quorum reached or Timeout after 20s
+                finishLatch.await(20, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                logger.error("Quorum timer failed");
             }
-            // TODO reach write quorum and only then issue client response
-            // Timeout after 20s checkear con un while meintras no tengan la repuesta y no pasen 20s
-            return false;
+            // reach read quorum with matching values and issue client response
+            int quorum = 1;
+            for (Response result : results) {
+                if (result.getSuccess()) {
+                    quorum++;
+                }
+            }
+            // reach write quorum and only then issue client response
+            if (quorum >= qwritesize) {
+                logger.debug("Data replication reached quorum");
+                return true;
+            } else {
+                logger.warn("Data replication failed. Quorum not reached");
+                return false;
+            }
         } else
-            // TODO send async. replication requests to nodes
-            for (HashMap.Entry<String, NodeClient> entry : otherServer.entrySet()) {
-                entry.getValue().replicate(key,value);
-            }
             // But already issue response to client
             return true;
     }
 
     /**
      * Method to fetch value from memory.
-     * 
+     * <p>
      * If quorum bigger than 1 is required, fetch replica values from other nodes.
      * You should also check if the returned values from the replicas are
      * consistent.
-     * 
+     * <p>
      * You are free to change this method if you want
      */
     private KeyValuePair gatherdata(String key) {
-        if (qreadsize > 1) {
-            // TODO reach read quorum with matching values and issue client response
-            // Timeout after 20s
+        Key request = Key.newBuilder().setKey(key).build();
+        List<Response> results = new ArrayList<Response>();
+        String data = Memory.get(key);
+        if (data == null) {
+            logger.warn("Uh oh, couldn't get data for key " + key);
             return null;
+        }
+        if (qreadsize > 1) {
+            final CountDownLatch finishLatch = new CountDownLatch(qreadsize - 1);
+            StreamObserver<Response> responses = new StreamObserver<Response>() {
+                @Override
+                public void onNext(Response response) {
+                    logger.info("Received replica " + response.getSuccess());
+                    if (response.getSuccess()) results.add(response);
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    logger.warn("Get Replica failed: " + t.getMessage());
+                    finishLatch.countDown();
+                }
+
+                @Override
+                public void onCompleted() {
+                    logger.info("Finished getReplica");
+                    finishLatch.countDown();
+                }
+            };
+
+            for (HashMap.Entry<String, KeyValueStoreStub> entry : otherServer.entrySet()) {
+                entry.getValue().getReplica(request, responses);
+            }
+            try {
+                // Wait until minimum quorum reached or Timeout after 20s
+                finishLatch.await(20, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                logger.error("Quorum timer failed");
+            }
+            // TODO reach read quorum with matching values and issue client response
+            int quorum = 1;
+            for (Response result : results) {
+                if (result.getSuccess() && data.equals(result.getValue())) {
+                    quorum++;
+                }
+            }
+            if (quorum >= qreadsize) {
+                logger.debug("Giving client the requested data");
+                return KeyValuePair.newBuilder().setKey(key).setValue(data).build();
+            } else {
+                logger.warn("Data is inconsistent");
+                return null;
+            }
         } else {
             // Reads local replica only and issues response
-            String data = Memory.get(key);
             if (data == null) {
                 logger.warn("Couldn't find data for key " + key);
                 return null;
